217b9a303bf2ff43c7d6ffaede507a488a16cf8b
José Eduardo Morales Garcia <edushowy@gmail.com>
2023-03-25 21:27:15 -0600

    agregado ejemplo de lectura y escritura de archivos txt , html y la presentacion de la clase 9

---
+++ Ejemplos Java/Ejemplos con NetBeans/manejoArchivos/src/manejoarchivos/ManejoArchivos.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package manejoarchivos;
+
+import interfazGrafica.App;
+
+/**
+ *
+ * @author edush
+ */
+public class ManejoArchivos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        App ventana = new App();
+        ventana.setVisible(true);
+    }
+    
+}
